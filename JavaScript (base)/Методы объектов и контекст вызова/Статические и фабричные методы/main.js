caa2704e252d8c3439db14b3dd0c5a942358882a
yaphtes <yaphtes.ks@gmail.com>
2016-10-18 21:27:17 +0300

    update 2: 18.10.16

---
+++ JavaScript (base)/Методы объектов и контекст вызова/Статические и фабричные методы/main.js
@@ -0,0 +1,23 @@
+// task 1
+// function Article() {
+//   this.created = new Date();
+//
+//   Article.count++;
+//   Article.last = this.created;
+// }
+// Article.count = 0;
+//
+//
+// Article.showStats = function() {
+//     alert('Всего' + this.count + 'Последняя: ' + this.last);
+//     console.log(this);
+// };
+//
+// new Article();
+// new Article();
+//
+// Article.showStats(); // Всего: 2, Последняя: (дата)
+//
+// new Article();
+//
+// Article.showStats(); // Всего: 3, Последняя: (дата)
