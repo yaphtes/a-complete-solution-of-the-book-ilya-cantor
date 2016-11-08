a11d703e22ee56621d905aa2c6e8df0fe7f7e23a
yaphtes <yaphtes.ks@gmail.com>
2016-11-08 23:42:00 +0300

    update: 08.11.16

--- JavaScript (base)/ООП в функциональном стиле/Геттеры и сеттеры/main.js
+++ JavaScript (base)/ООП в функциональном стиле/Геттеры и сеттеры/main.js
@@ -1,11 +1,16 @@
 'use strict';
 
 function CoffeeMachine(power, capacity) {
-	var waterAmount = 0;
+	var waterAmount         = 0;
 	var WATER_HEAT_CAPACITY = 4200;
+	var timerId;
 
 	function getBoilTime() {
 		return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+	}
+
+	function onReady() {
+		alert('Кофе готово!');
 	}
 
 	this.setWaterAmount = function(amount) {
@@ -18,19 +23,90 @@ function CoffeeMachine(power, capacity) {
 		}
 	};
 
+	this.setOnReady = function(newOnReady) {
+		onReady = newOnReady;
+	};
+
 	this.getWaterAmount = function() {
 		return waterAmount;
-	}
+	};
 
-	function onReady() {
-		alert('Кофе готово!');
-	}
+	this.getPower = function() {
+		return power;
+	};
+
+	this.addWater = function(amount) {
+		this.setWaterAmount(waterAmount + amount);
+	};
 
 	this.run = function() {
-		setTimeout( onReady, getBoilTime() );
+		timerId = setTimeout(function() {
+			timerId = null;
+			onReady();
+		}, getBoilTime());
 	};
+
+	this.isRunning = function() {
+		return Boolean(timerId);
+	}
 }
 
-var coffeeMachine = new CoffeeMachine(100000, 500);
-coffeeMachine.setWaterAmount(450);
-alert( coffeeMachine.getWaterAmount() );
+// task 5
+var coffeeMachine = new CoffeeMachine(20000, 500);
+coffeeMachine.setWaterAmount(100);
+
+alert( 'До: ' + coffeeMachine.isRunning() ); // До: false
+
+coffeeMachine.run();
+alert( 'В процессе: ' + coffeeMachine.isRunning() ); // В процессе: true
+
+coffeeMachine.setOnReady(function() {
+	alert( "После: " + coffeeMachine.isRunning() ); // После: false
+});
+
+// task 4
+// var coffeeMachine = new CoffeeMachine(20000, 500);
+// coffeeMachine.setWaterAmount(150);
+//
+// coffeeMachine.setOnReady(function() {
+// 	var amount = coffeeMachine.getWaterAmount();
+// 	alert( 'Готов кофе: ' + amount + 'мл' ); // Кофе готов: 150 мл
+// });
+//
+// coffeeMachine.run();
+
+// task 3
+// var coffeeMachine = new CoffeeMachine(100000, 400);
+// coffeeMachine.addWater(200);
+// coffeeMachine.addWater(100);
+// coffeeMachine.addWater(300); // Нельзя залить больше, чем 400
+// coffeeMachine.run();
+
+// var coffeeMachine = new CoffeeMachine(100000, 500);
+// coffeeMachine.setWaterAmount(450);
+// alert( coffeeMachine.getWaterAmount() );
+
+
+// task 2
+// function User() {
+// 	var firstName = '';
+// 	var surname = '';
+//
+// 	this.setFirstName = function(value) {
+// 		firstName = value;
+// 	};
+//
+// 	this.setSurname = function(value) {
+// 		surname = value;
+// 	};
+//
+// 	this.getFullName = function() {
+// 		return `${firstName} ${surname}`;
+// 	};
+// }
+//
+// var user = new User();
+// user.setFirstName("Петя");
+// user.setSurname("Иванов");
+//
+// alert( user.getFullName() ); // Петя Иванов
