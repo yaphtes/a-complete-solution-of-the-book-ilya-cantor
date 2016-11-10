3aad5555a624d86c3cc813d4ba1c8e0f5a485cfd
yaphtes <yaphtes.ks@gmail.com>
2016-11-10 14:49:51 +0300

    update: 10.11.16

--- JavaScript (base)/ООП в прототипном стиле/Встроенные "классы" в JavaScript/main.js
+++ JavaScript (base)/ООП в прототипном стиле/Встроенные "классы" в JavaScript/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// task 1
+// Function.prototype.defer = function(ms) {
+//   setTimeout(this, ms);
+// }
+//
+// function f() {
+//   alert( "привет" );
+// }
+//
+// f.defer(1000); // выведет "привет" через 1 секунду
+
+
+
+// task 2
+Function.prototype.defer = function(ms) {
+	var f = this;
+	return function() {
+		var self = this;
+		var args = arguments;
+
+		setTimeout(function() {
+			f.apply(self, args);
+		}, ms);
+	};
+}
+
+function f(a, b) {
+	alert(a + b);
+}
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
--- JavaScript (base)/ООП в прототипном стиле/Прототип объекта/main.js
+++ JavaScript (base)/ООП в прототипном стиле/Прототип объекта/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+// task 1
+var animal = {
+	jumps: null
+};
+var rabbit = {
+	jumps: true
+};
+
+// rabbit.__proto__ = animal;
+Object.setPrototypeOf(rabbit, animal);
+
+alert( rabbit.jumps ); // true (1)
+
+delete rabbit.jumps;
+
+alert( rabbit.jumps ); // null (2)
+
+delete animal.jumps;
+
+alert( rabbit.jumps ); // undefined (3)
+
+
+
+// task 3
+var head = {
+	glasses: 1
+};
+
+var table = {
+	pen: 3
+};
+Object.setPrototypeOf(table, head);
+
+var bed = {
+	sheet: 1,
+	pillow: 2
+};
+Object.setPrototypeOf(bed, table);
+
+var pockets = {
+	money: 2000
+};
+Object.setPrototypeOf(pockets, bed);
+alert( pockets.pen ); // 3
+alert( bed.glasses ); // 1
+alert( table.money ); // undefined
