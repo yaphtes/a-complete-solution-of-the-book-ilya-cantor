7575e7d18ca57e2b280109ee27f1a6bc89c2540d
yaphtes <yaphtes.ks@gmail.com>
2016-11-16 00:47:56 +0300

    update: 16.11.16

---
+++ tests/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// let group = {
+// 	title: "Наш курс",
+// 	students: ["Вася", "Петя", "Даша"],
+//
+// 	showList: function() {
+// 		this.students.forEach(
+// 			student => alert(this.title + ': ' + student)
+// 		)
+// 	}
+// }
+//
+// group.showList();
+// // Наш курс: Вася
+// // Наш курс: Петя
+// // Наш курс: Даша
+
+let name = 'John';
+
+const obj = {
+	name
+};
+
+console.log(obj.name);
